31bbacd3b797f3ce023fa0ee3366cee4b5333107
Pedro López <pedrolopez.e@hotmail.com>
2022-06-02 19:22:27 -0300

    Write the entity

---
+++ binary_counter.vhd
@@ -0,0 +1,13 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+entity binary_counter is
+    port (
+        clk : in std_logic;
+        q_reg : out std_logic;
+        q_BCD : out std_logic; 
+    );
+    end;
+
+architecture binary_counter_arch of binary_counter is
+end;
