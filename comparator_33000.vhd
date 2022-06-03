7d124d27a6800bc686b5d33faa7771cc4166fa15
Pedro López <pedrolopez.e@hotmail.com>
2022-06-03 20:01:05 -0300

    Correct ffd

--- comparator_33000.vhd
+++ comparator_33000.vhd
@@ -1,3 +1,12 @@
 library IEEE;
 use IEEE.std_logic_1164.all;
 
+entity comparator_33000 is 
+    port (
+        
+
+    );
+    end;
+
+architecture comparator_33000_arch of comparator_33000 is
+end;
--- ffd.vhd
+++ ffd.vhd
@@ -7,14 +7,19 @@ entity ffd is
         clk : in std_logic;
         ena : in std_logic;
         q   : out std_logic;
+        rst : in std_logic;
     );
 end;
 
 architecture ffd_arch of ffd is 
 begin 
     process(clk, ena)
-        if (clk = "1") & (ena = '1') then
-            q <= d 
+        if rising_edge(clk) then
+            if rst = '1' then
+                q <= '0';
+            elsif ena = '1' then 
+               q <= d; 
+            end if;
         end if;
     end process;
 end;
