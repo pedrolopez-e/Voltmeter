52e942c02a2cf88ad187dd91dd2cba3d621b8b9a
Pedro López <pedrolopez.e@hotmail.com>
2022-06-05 17:29:47 -0300

    Entity comparator

--- comparator_33000.vhd
+++ comparator_33000.vhd
@@ -3,7 +3,9 @@ use IEEE.std_logic_1164.all;
 
 entity comparator_33000 is 
     port (
-        
+        A : in std_logic_vector(21 downto 0);
+        B : in std_logic_vector(21 downto 0);
+        q : out std_logic
     );
     end;
 
